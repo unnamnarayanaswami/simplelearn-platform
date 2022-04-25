20436e840cd86a54d7e93263232fe060f2b4a3ac
unnamnarayanaswami <99788351+unnamnarayanaswami@users.noreply.github.com>
2022-04-25 22:00:36 +0530

    Create SpringBootStarterApplication.java

---
+++ phase3/Assisted Projects/SpringBootStarter/src/test/java/com/ecommerce/SpringBootStarterApplication.java
@@ -0,0 +1,16 @@
+package com.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+© 2022 GitHub, Inc.
+Terms
+Priva
