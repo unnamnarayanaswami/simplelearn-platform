d7fd38c32274724274774e2bfd5380e85721d020
unnamnarayanaswami <99788351+unnamnarayanaswami@users.noreply.github.com>
2022-04-27 11:09:58 +0530

    Create SpringSslApplicationTests.java

---
+++ phase3/Assisted Projects/SpringSSL/src/test/java/com/ecommerce/SpringSslApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+© 2022 GitHub, Inc.
+Terms
